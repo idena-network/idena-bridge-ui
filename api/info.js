531eafd6b450799ff32240aee1dc45318c20c38f
ridenaio <r@idena.io>
2021-03-04 20:19:44 +0500

    Change route

--- api/info.js
+++
@@ -1,8 +0,0 @@
-import { createProxyMiddleware } from 'http-proxy-middleware'
-
-export default createProxyMiddleware({
-    changeOrigin: false,
-    secure: false,
-    target: process.env.API_URL,
-    pathRewrite: function (path, req) { return path.replace('/api', '/swaps') }
-})
