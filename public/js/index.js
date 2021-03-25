0bcc9cb31b1ceebfdcc663c661c0acc038737a8c
RealMahmoud <realmahmoudm@gmail.com>
2021-03-25 13:52:11 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function updateCardsLinks() {
     document.getElementById("bsc-link").href = global_variables.BSC_EXPLORER + "/token/" + global_variables.BSC_CONTRACT;
-    document.getElementById("idena-link").href = global_variables.IDENA_EXPLORER + "/address/ " + global_variables.IDENA_WALLET;
+    document.getElementById("idena-link").href = global_variables.IDENA_EXPLORER + "/address/" + global_variables.IDENA_WALLET;
 }
 
 function fetchData() {
