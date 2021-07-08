0745138d861056cc6372939be8001ea2050554ea
mbidenaio <mb@idena.io>
2021-07-08 16:43:36 +0500

    Allow to retry to submit idena tx in case of error

--- public/js/submit.js
+++ public/js/submit.js
@@ -4,7 +4,13 @@ document.addEventListener('DOMContentLoaded', function () {
     var tx = params.searchParams.get("tx");
 
     if (uuid && tx) {
-        document.getElementById("redirect-text").innerHTML = "Submiting...";
+        document.getElementById("retry-btn").onclick = function () {
+            document.getElementById("redirect-text2").innerHTML = '';
+            document.getElementById("retry-btn").classList.add("d-none");
+            document.getElementById("redirect-text").innerHTML = "Submitting...";
+            submit(uuid, tx);
+        }
+        document.getElementById("redirect-text").innerHTML = "Submitting...";
         submit(uuid, tx);
     } else {
         document.getElementById("redirect-text").innerHTML = "Missing parameters";
@@ -21,8 +27,11 @@ function submit(uuid, tx) {
             document.getElementById("redirect-text").innerHTML = "Redirecting...";
             window.location.replace(`/operation/${uuid}`)
         }).catch(function (err) {
-            document.getElementById("redirect-text").innerHTML = 'Something went wrong.';
-            document.getElementById("redirect-text2").innerHTML = 'Please submit the TxHash manually.';
-        });
+        document.getElementById("redirect-text").innerHTML = 'Something went wrong.';
+        if (err.response.status !== 400) {
+            document.getElementById("redirect-text2").innerHTML = 'Please try again in few seconds.';
+            document.getElementById("retry-btn").classList.remove("d-none");
+        }
+    });
 
 }
